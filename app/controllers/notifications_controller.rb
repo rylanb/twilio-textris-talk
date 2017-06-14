02477bce3a1489de7c60cddca6cc44013097cc01
Noel Worden <noelworden@gmail.com>
2017-06-13 20:50:07 -0600

    [CHG] ---------------------------------------- Modified the flash messages so they work

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,12 +2,12 @@ class NotificationsController < ApplicationController
 
   def create
     AgentTexter.alert(params, user_phone).deliver
-    redirect_to root_url,
-      success: 'Success! Your phone should be pinged any second now'
+    redirect_to root_url
+      flash[:notice] = 'Success! Your phone should be pinged any second now'
   rescue Twilio::REST::RequestError => error
     p error.message
     redirect_to root_url,
-      error: 'Oops! There was an error. Please try again.'
+      flash[:danger] = 'Oops! There was an error. Please try again.'
   end
 
   private
