6be9acb2f4b5d4dc5a585f53794a03856cc21e61
Rylan Bowers <rylanb@gmail.com>
2017-06-13 23:01:22 -0600

    Removing puts in talk_texter.

--- app/texters/talk_texter.rb
+++ app/texters/talk_texter.rb
@@ -2,7 +2,6 @@ class TalkTexter < Textris::Base
   default :from => ENV["TWILIO_NUMBER"]
 
   def alert(name, user_phone)
-    puts "name: #{name}. Phone: #{user_phone}\n\n"
     @name = name
     text to: user_phone
   end
