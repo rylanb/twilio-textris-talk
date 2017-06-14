97fb11beca9acbdcbe53026f263c58d88b80e343
Rylan Bowers <rylanb@gmail.com>
2017-06-13 23:30:01 -0600

    Adding twilio to send for textris to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.textris_delivery_method = :twilio
 end
